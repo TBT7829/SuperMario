91cc5c4bfcc6da1248a1546096e538c977940d65
504-40\cre <a>
2026-06-06 19:36:28 +0900

    クリボークラスの作成

---
+++ Base/DxLibTemplate_ver0.1/DxLibTemplate_ver0.1/Goomba.cpp
@@ -0,0 +1,20 @@
+#include "Goomba.h"
+#include "Enemy.h"
+#include "EnemyManager.h"
+#include <DxLib.h>
+
+Goomba::Goomba()
+{
+}
+
+Goomba::~Goomba()
+{
+}
+
+void Goomba::update()
+{
+}
+
+void Goomba::render()
+{
+}
--- Base/DxLibTemplate_ver0.1/DxLibTemplate_ver0.1/Goomba.h
+++ Base/DxLibTemplate_ver0.1/DxLibTemplate_ver0.1/Goomba.h
@@ -1,1 +1,20 @@
-#pragma once
+#pragma 
+
+class Goomba : public Enemy
+{
+public:
+	// ’l‚ğ“ü‚ê‚é
+	const float GOOMBA_MOVE_SPEED;
+
+	Goomba();
+	virtual ~Goomba();
+
+	//! @brief XVˆ—
+	void update() override;
+
+	//! @brief •`‰æˆ—
+	void render() override;
+
+
+};
+
